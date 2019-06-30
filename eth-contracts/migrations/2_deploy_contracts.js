9de2f637b9db948c2bee8d610fb3744fd517d4a0
Ujjwal Barapatre <46401115+UjjwalKB@users.noreply.github.com>
2019-06-30 18:15:42 +0530

    Add files via upload

---
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -0,0 +1,10 @@
+var Verifier = artifacts.require("Verifier");
+var SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
+
+module.exports = function(deployer) {
+
+  deployer.deploy(Verifier)
+    .then(() => {
+        return deployer.deploy(SolnSquareVerifier, Verifier.address);
+    });
+};
